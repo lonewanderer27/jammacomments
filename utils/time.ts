bb5ae0c9de6358791344c99f91cf921e5d27a6ff
Adriane James J. Puzon <adrianejames27@gmail.com>
2022-12-05 18:23:39 +0800

    add getCurrentDateString

---
+++ utils/time.ts
@@ -0,0 +1,16 @@
+export function getCurrentDateString(): string {
+  const m = new Date();
+  const dateString =
+    m.getUTCFullYear() +
+    "-" +
+    ("0" + (m.getUTCMonth() + 1)).slice(-2) +
+    "-" +
+    ("0" + m.getUTCDate()).slice(-2) +
+    " " +
+    ("0" + m.getHours()).slice(-2) +
+    ":" +
+    ("0" + m.getUTCMinutes()).slice(-2) +
+    ":" +
+    ("0" + m.getUTCSeconds()).slice(-2);
+  return dateString;
+}
